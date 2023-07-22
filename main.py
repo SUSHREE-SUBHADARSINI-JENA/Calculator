f146bcea0de4e1c6917223605e7ed172b93e38e4
SUSH.S. J <138697286+SUSHREE-SUBHADARSINI-JENA@users.noreply.github.com>
2023-07-22 12:32:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    pass
+first = input("enter first number: ")
+operator = input("enter operator (+,-,*,/,%) : ")
+second = input("enter second number: ")
+first = int(first)
+second = int(second)
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "/":
+    print(first / second)
+elif operator == "%":
+    print(first % second)
+else:
+    print("invalid operations")
